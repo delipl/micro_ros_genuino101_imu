892150b75e5998bbacce57f7f1e2672c468e04d5
Jakub Delicat <jakub.delicat@husarion.com>
2022-10-10 19:46:45 +0200

    Got acceleration and gyration in SI

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Genuino101 dfu linux
 
 ```bash
-$ wget https://github.com/01org/intel-arduino-tools/raw/linux64/scripts/create_dfu_udev_rule
-$ chmod +x create_dfu_udev_rule
-$ sudo ./create_dfu_udev_rule
-$ rm create_dfu_udev_rule
+wget https://github.com/01org/intel-arduino-tools/raw/linux64/scripts/create_dfu_udev_rule
+chmod +x create_dfu_udev_rule
+sudo ./create_dfu_udev_rule
+rm create_dfu_udev_rule
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
+
 #include "CurieIMU.h"
 
-int ax, ay, az;  // accelerometer values
-int gx, gy, gz;  // gyrometer values
-const int ledPin = 13;  // activity LED pin
-bool blinkState = false;  // state of the LED
-int calibrateOffsets = 1;  // int to determine whether calibration takes place or not
+int raw_acceleration_x, raw_acceleration_y, raw_acceleration_z;  // accelerometer values
+int raw_gyration_x, raw_gyration_y, raw_gyration_z;              // gyrometer values
+const int ledPin = 13;                                           // activity LED pin
+bool blinkState = false;                                         // state of the LED
+int calibrateOffsets = 1;                                        // int to determine whether calibration takes place or not
+float dt = 0.0;
 
 void setup() {
     Serial.begin(9600);  // initialize Serial communication
@@ -33,9 +35,8 @@ void setup() {
         Serial.print(CurieIMU.getGyroOffset(X_AXIS));
         Serial.print("\t");  // 0
         Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
-       Serial.print("\t");  // 0
+        Serial.print("\t");  // 0
         Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
-
 
         Serial.println("About to calibrate. Make sure your board is stable and upright");
 
@@ -66,40 +67,79 @@ void setup() {
     // configure Arduino LED for activity indicator
 
     pinMode(ledPin, OUTPUT);
+    dt = millis();
+}
+
+float acceleration_x;
+float acceleration_y;
+float acceleration_z;
+float gyration_x;
+float gyration_y;
+float gyration_z;
+
+float yaw = 0.0;
+float pitch = 0.0;
+float roll = 0.0;
+
+int counter = 0;
+
+
+void read_and_accumulate_measurements(){
+    int read_acceleration_x, read_acceleration_y, read_acceleration_z;
+    int read_gyration_x, read_gyration_y, read_gyration_z;
+
+    CurieIMU.readAccelerometer(read_acceleration_x, read_acceleration_y, read_acceleration_z);
+    CurieIMU.readGyro(read_gyration_x, read_gyration_y, read_gyration_z);
+    raw_acceleration_x += read_acceleration_x;
+    raw_acceleration_y += read_acceleration_y;
+    raw_acceleration_z += read_acceleration_z;
+
+    raw_gyration_x += read_gyration_x;
+    raw_gyration_y += read_gyration_y;
+    raw_gyration_z += read_gyration_z;
+}
+
+void calculate_acceleration_and_gyration(){
+    if (counter == 50) {
+        dt = (millis() - dt) / 1000;
+        acceleration_x = (raw_acceleration_x / 32768.0 / counter) * CurieIMU.getAccelerometerRange() * 9.80665;
+        acceleration_y = (raw_acceleration_y / 32768.0 / counter) * CurieIMU.getAccelerometerRange() * 9.80665;
+        acceleration_z = (raw_acceleration_z / 32768.0 / counter) * CurieIMU.getAccelerometerRange() * 9.80665;
+        gyration_x = (raw_gyration_x / 32768.9 / counter) * CurieIMU.getGyroRange() * 3.14159265359 / 180.0;
+        gyration_y = (raw_gyration_y / 32768.9 / counter) * CurieIMU.getGyroRange() * 3.14159265359 / 180.0;
+        gyration_z = (raw_gyration_z / 32768.9 / counter) * CurieIMU.getGyroRange() * 3.14159265359 / 180.0;
+
+        Serial.print("a/g:\t");
+        Serial.print(gyration_x);
+        Serial.print("\t");
+        Serial.print(gyration_y);
+        Serial.print("\t");
+        Serial.print(gyration_z);
+        Serial.print("\t");
+        Serial.print(acceleration_x);
+        Serial.print("\t");
+        Serial.print(acceleration_y);
+        Serial.print("\t");
+        Serial.println(acceleration_z);
+
+        blinkState = !blinkState;
+        digitalWrite(ledPin, blinkState);
+        ;
+
+        raw_gyration_x = 0;
+        raw_gyration_y = 0;
+        raw_gyration_z = 0;
+        raw_acceleration_x = 0;
+        raw_acceleration_y = 0;
+        raw_acceleration_z = 0;
+        counter = 0;
+    }
 }
 
 void loop() {
-    // read raw accel/gyro measurements from device
+    read_and_accumulate_measurements();
+    calculate_acceleration_and_gyration();
 
-    CurieIMU.readMotionSensor(ax, ay, az, gx, gy, gz);
-
-    Serial.print("a/g:\t");
-
-    Serial.print(ax);
-
-    Serial.print("\t");
-
-    Serial.print(ay);
-
-    Serial.print("\t");
-
-    Serial.print(az);
-
-    Serial.print("\t");
-
-    Serial.print(gx);
-
-    Serial.print("\t");
-
-    Serial.print(gy);
-
-    Serial.print("\t");
-
-    Serial.println(gz);
-
-    // blink LED to indicate activity
-
-    blinkState = !blinkState;
-
-    digitalWrite(ledPin, blinkState);
+    delay(10);
+    ++counter;
 }
